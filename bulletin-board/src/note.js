d2836b3362bd13cb527a5b809f1c8f8e9ffe62e4
Rahsaan Basek <rahsaanbasek@gmail.com>
2018-03-26 18:05:52 -0400

    update bulletin-board-notes.txt

--- bulletin-board/src/note.js
+++ bulletin-board/src/note.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppy0 from 'react-icons/lib/fa/Floppy-0'
 
 class Note extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            editing: false
+        }
+
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+
     }
 
     edit() {
+        this.setState ({
+            editing: true
+        })
+
         alert('editing note')
     }
 
@@ -17,7 +30,24 @@ class Note extends React.Component {
         alert('removing note')
     }
 
-    render() {
+    save(){
+        alert('saved!')
+    }
+
+    renderForm(){
+    return(
+        <div className="note">
+           <form> 
+            <text area /> 
+                <button onClick = {this.save} ><FaFloppy0 /></button>
+            </form>
+        </div>
+        )
+    }
+
+
+    
+    renderDisplay() {
         return (
             <div className="note">
                 <p>Learn React</p>
@@ -30,6 +60,14 @@ class Note extends React.Component {
         )
     }
 
-}
+    render() {
+		if(this.state.editing){
+            return this.render.form()
+        } else {
+            return this.renderDisplay()
+        }
+	}
+
+
 
 export default Note
